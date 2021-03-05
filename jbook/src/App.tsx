2febc8487d6d94fbddb46eaefafab18d817e4b69
mkoenke <mkoenke@sas.upenn.edu>
2021-03-05 16:21:27 -0500

    add esbuild for transpiling

--- jbook/src/App.tsx
+++ jbook/src/App.tsx
@@ -1,11 +1,31 @@
-import { useState } from 'react'
+import * as esbuild from 'esbuild-wasm'
+import { useEffect, useRef, useState } from 'react'
 
 const App = () => {
+  const ref = useRef<any>()
   const [input, setInput] = useState('')
   const [code, setCode] = useState('')
 
-  const onClick = () => {
-    console.log(input)
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/asbuild.wasm',
+    })
+  }
+  useEffect(() => {
+    startService()
+  }, [])
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return
+    }
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    })
+
+    setCode(result.code)
   }
 
   return (
