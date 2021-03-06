474f5c9bfcf46f07391a08fded5f8c2741ad6051
mkoenke <mkoenke@sas.upenn.edu>
2021-03-06 16:26:23 -0500

    add plugin

--- jbook/src/App.tsx
+++ jbook/src/App.tsx
@@ -1,5 +1,6 @@
 import * as esbuild from 'esbuild-wasm'
 import { useEffect, useRef, useState } from 'react'
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
 
 const App = () => {
   const ref = useRef<any>()
@@ -20,12 +21,19 @@ const App = () => {
     if (!ref.current) {
       return
     }
-    const result = await ref.current.transform(input, {
-      loader: 'jsx',
-      target: 'es2015',
+    // const result = await ref.current.transform(input, {
+    //   loader: 'jsx',
+    //   target: 'es2015',
+    // })
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
     })
+    // console.log(result)
 
-    setCode(result.code)
+    setCode(result.outputFiles[0].text)
   }
 
   return (
