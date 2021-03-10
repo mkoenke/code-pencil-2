c4416f6f079a8fdb352f8e2c3d2b69be18b336e7
mkoenke <mkoenke@sas.upenn.edu>
2021-03-10 15:24:59 -0500

    add monaco code editor

--- jbook/src/App.tsx
+++ jbook/src/App.tsx
@@ -1,5 +1,6 @@
 import * as esbuild from 'esbuild-wasm'
 import { useEffect, useRef, useState } from 'react'
+import CodeEditor from './components/code-editor'
 import { fetchPlugin } from './plugins/fetch-plugin'
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
 
@@ -58,6 +59,7 @@ const App = () => {
   `
   return (
     <div>
+      <CodeEditor />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
