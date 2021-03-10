686787c49ed915e34d4ff4ba40727e38405f5206
mkoenke <mkoenke@sas.upenn.edu>
2021-03-10 15:11:11 -0500

    add reset of iframe contents

--- jbook/src/App.tsx
+++ jbook/src/App.tsx
@@ -7,7 +7,6 @@ const App = () => {
   const ref = useRef<any>()
   const iframe = useRef<any>()
   const [input, setInput] = useState('')
-  const [code, setCode] = useState('')
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -23,6 +22,9 @@ const App = () => {
     if (!ref.current) {
       return
     }
+
+    iframe.current.srcdoc = html
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -42,7 +44,13 @@ const App = () => {
         <div id="root"></div>
         <script>
           window.addEventListener('message', (event) => {
-            eval(event.data);
+            try{
+              eval(event.data);
+            } catch(err){
+              const root = document.querySelector('#root')
+              root.innerHTML = '<div style="color: red;" ><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err) 
+            }
           }, false);
         </script>
       </body>
@@ -57,8 +65,12 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
-      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+      <iframe
+        title="preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
     </div>
   )
 }
