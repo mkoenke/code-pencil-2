86be8afeb3b573076987e0866e8069ece2ca2a0e
mkoenke <mkoenke@sas.upenn.edu>
2021-03-10 12:30:53 -0500

    add fix to wasmURL

--- jbook/src/App.tsx
+++ jbook/src/App.tsx
@@ -10,7 +10,7 @@ const App = () => {
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/asbuild.wasm',
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     })
   }
   useEffect(() => {
