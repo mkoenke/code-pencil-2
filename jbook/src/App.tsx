180c69759aea7c1e7c59300e0f554eae4a80ca4a
mkoenke <mkoenke@sas.upenn.edu>
2021-03-10 14:48:34 -0500

    add window event listener to communicate between input and iframe

--- jbook/src/App.tsx
+++ jbook/src/App.tsx
@@ -5,6 +5,7 @@ import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
 
 const App = () => {
   const ref = useRef<any>()
+  const iframe = useRef<any>()
   const [input, setInput] = useState('')
   const [code, setCode] = useState('')
 
@@ -32,10 +33,21 @@ const App = () => {
         global: 'window',
       },
     })
-
-    setCode(result.outputFiles[0].text)
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
   }
-  const html = `<script>${code}</script>`
+  const html = `
+  <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            eval(event.data);
+          }, false);
+        </script>
+      </body>
+    </html>
+  `
   return (
     <div>
       <textarea
@@ -46,7 +58,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
-      <iframe sandbox="allow-scripts" srcDoc={html} />
+      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
     </div>
   )
 }
