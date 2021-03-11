c1a0d29e1d07989106e551d6a18264d0c2978f65
mkoenke <mkoenke@sas.upenn.edu>
2021-03-11 17:30:45 -0500

    add text editor with toggling

--- jbook/src/App.tsx
+++ jbook/src/App.tsx
@@ -1,9 +1,11 @@
-import CodeCell from './components/code-cell'
+// import CodeCell from './components/code-cell'
+import TextEditor from './components/text-editor'
 
 const App = () => {
   return (
     <div>
-      <CodeCell />
+      <TextEditor />
+      {/* <CodeCell /> */}
     </div>
   )
 }
