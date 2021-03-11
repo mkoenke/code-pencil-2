c933a734f001d308806b58c137cbb5ac576a8061
mkoenke <mkoenke@sas.upenn.edu>
2021-03-11 17:51:21 -0500

    add styling to text editor

--- jbook/src/components/text-editor.tsx
+++ jbook/src/components/text-editor.tsx
@@ -1,9 +1,11 @@
 import MDEditor from '@uiw/react-md-editor'
 import { useEffect, useRef, useState } from 'react'
+import './text-editor.css'
 
 const TextEditor: React.FC = () => {
   const ref = useRef<HTMLDivElement | null>(null)
   const [editing, setEditing] = useState(false)
+  const [value, setValue] = useState('# Header')
 
   useEffect(() => {
     const listener = (event: MouseEvent) => {
@@ -24,14 +26,16 @@ const TextEditor: React.FC = () => {
 
   if (editing) {
     return (
-      <div ref={ref}>
-        <MDEditor />
+      <div ref={ref} className="text-editor">
+        <MDEditor value={value} onChange={(v) => setValue(v || '')} />
       </div>
     )
   }
   return (
-    <div onClick={() => setEditing(true)}>
-      <MDEditor.Markdown source={'# Header'} />
+    <div className="text-editor card" onClick={() => setEditing(true)}>
+      <div className="card-content">
+        <MDEditor.Markdown source={value} />
+      </div>
     </div>
   )
 }
