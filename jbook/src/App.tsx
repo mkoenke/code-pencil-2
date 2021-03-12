5f34d107268be50e0361c9f8f83a747c20e68fdc
mkoenke <mkoenke@sas.upenn.edu>
2021-03-12 11:06:32 -0500

    add cell reducer logic

--- jbook/src/App.tsx
+++ jbook/src/App.tsx
@@ -1,12 +1,16 @@
 // import CodeCell from './components/code-cell'
+import { Provider } from 'react-redux'
 import TextEditor from './components/text-editor'
+import { store } from './state'
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-      {/* <CodeCell /> */}
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+        {/* <CodeCell /> */}
+      </div>
+    </Provider>
   )
 }
 
