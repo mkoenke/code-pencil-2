1df7bbef7ce53440c7cf0e222dd32fa772453696
mkoenke <mkoenke@sas.upenn.edu>
2021-03-14 13:28:32 -0400

    add typescript support to local-api

--- jbook/src/index.tsx
+++
@@ -1,6 +0,0 @@
-import '@fortawesome/fontawesome-free/css/all.min.css'
-import 'bulmaswatch/superhero/bulmaswatch.min.css'
-import ReactDOM from 'react-dom'
-import App from './App'
-
-ReactDOM.render(<App />, document.getElementById('root'))
