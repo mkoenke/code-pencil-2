de011199a61b93e6a1a03f652f722fa6984a7602
mkoenke <mkoenke@sas.upenn.edu>
2021-03-15 12:20:06 -0400

    add routes to local-api

--- code-pencil/packages/local-api/dist/index.js
+++ code-pencil/packages/local-api/dist/index.js
@@ -5,19 +5,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 var express_1 = __importDefault(require("express"));
-// import {createProxyMiddleware} from "http-proxy-middleware"
+var http_proxy_middleware_1 = require("http-proxy-middleware");
 var path_1 = __importDefault(require("path"));
-var serve = function (port, filename, dir) {
+var cells_1 = require("./routes/cells");
+var serve = function (port, filename, dir, useProxy) {
     var app = express_1.default();
-    var packagePath = require.resolve('local-client/build/index.html');
-    app.use(express_1.default.static(path_1.default.dirname(packagePath)));
-    // app.use(
-    //   createProxyMiddleware({
-    //     target: 'http://localhost:3000',
-    //     ws: true,
-    //     logLevel: 'silent',
-    //   })
-    // )
+    if (useProxy) {
+        app.use(http_proxy_middleware_1.createProxyMiddleware({
+            target: 'http://localhost:3000',
+            ws: true,
+            logLevel: 'silent',
+        }));
+    }
+    else {
+        var packagePath = require.resolve('local-client/build/index.html');
+        app.use(express_1.default.static(path_1.default.dirname(packagePath)));
+    }
+    app.use(cells_1.createCellsRouter(filename, dir));
     return new Promise(function (resolve, reject) {
         app.listen(port, resolve).on('error', reject);
     });
